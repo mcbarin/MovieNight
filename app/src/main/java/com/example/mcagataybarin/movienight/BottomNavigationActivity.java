c946eaedc720acd15cc59b30bc836ba5da4f3cea
Mehmet Çağatay Barın <mcagataybarin@gmail.com>
2017-03-08 22:21:34 +0300

    Seperate fragments for Bottom Navigation Activity created.

--- app/src/main/java/com/example/mcagataybarin/movienight/BottomNavigationActivity.java
+++ app/src/main/java/com/example/mcagataybarin/movienight/BottomNavigationActivity.java
@@ -1,13 +1,19 @@
 package com.example.mcagataybarin.movienight;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationBuilderWithBuilderAccessor;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class BottomNavigationActivity extends AppCompatActivity {
+public class BottomNavigationActivity extends AppCompatActivity
+        implements HomeFragment.OnFragmentInteractionListener, ProfileFragment.OnFragmentInteractionListener,
+        NotificationFragment.OnFragmentInteractionListener {
 
     private TextView mTextMessage;
 
@@ -16,18 +22,22 @@ public class BottomNavigationActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
+                    selectedFragment = HomeFragment.newInstance("param1", "param2");
+                    break;
                 case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
+                    selectedFragment = ProfileFragment.newInstance("param1", "param2");
+                    break;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    selectedFragment = NotificationFragment.newInstance("param1", "param2");
+                    break;
             }
-            return false;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.content, selectedFragment);
+            transaction.commit();
+            return true;
         }
 
     };
@@ -37,9 +47,13 @@ public class BottomNavigationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+        //you can leave it empty
     }
 
 }
