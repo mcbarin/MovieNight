6727fa830de37868cc05099c17793b85847bf2c7
Mehmet Çağatay Barın <mcagataybarin@gmail.com>
2017-03-16 02:41:29 +0300

    Movie information can be seen on movie fragment_ except image

--- app/src/main/java/com/example/mcagataybarin/movienight/MovieRecyclerViewAdapter.java
+++ app/src/main/java/com/example/mcagataybarin/movienight/MovieRecyclerViewAdapter.java
@@ -1,15 +1,22 @@
 package com.example.mcagataybarin.movienight;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.mcagataybarin.movienight.MovieFragment.OnListFragmentInteractionListener;
 import com.example.mcagataybarin.movienight.dummy.DummyContent.DummyItem;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,7 +47,9 @@ public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecycler
         holder.mItem = upcoming_movies.get(position);
         holder.mIdView.setText(upcoming_movies.get(position).title);
         holder.mContentView.setText(upcoming_movies.get(position).detail);
-
+        Drawable image = loadImageFromWebOperations(upcoming_movies.get(position).image);
+        if(image != null)
+            holder.mImageView.setImageDrawable(image);
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,15 +60,28 @@ public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecycler
         });
     }
 
+    private Drawable loadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, "Movie Image");
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     @Override
     public int getItemCount() {
         return upcoming_movies.size();
     }
 
+
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         public final TextView mIdView;
         public final TextView mContentView;
+        public final ImageView mImageView;
         public Movie mItem;
 
         public ViewHolder(View view) {
@@ -67,6 +89,7 @@ public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecycler
             mView = view;
             mIdView = (TextView) view.findViewById(R.id.id);
             mContentView = (TextView) view.findViewById(R.id.content);
+            mImageView = (ImageView) view.findViewById(R.id.imageView);
         }
 
         @Override
