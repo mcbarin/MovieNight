253578a029f46de60a43b6fab832a53c679a93e5
Mehmet Çağatay Barın <mcagataybarin@gmail.com>
2017-05-25 01:20:22 +0300

    Upcoming movies script is modified

--- upcoming_movies.py
+++ upcoming_movies.py
@@ -6,23 +6,50 @@ from bs4 import BeautifulSoup
 import json
 
 def movie_details(movie):
-    title = movie.find("a", {"class": "no_underline"}).text.replace("\n", "")
-    image = movie.find("img").get("src")
-    detail = movie.find("p").text.replace("\n", "")
-    date = movie.findAll("div", {"class": "oflow_a"})[0].text.split("\n")[1]
-    duration = movie.findAll("div", {"class": "oflow_a"})[0].text.split("\n")[2][1:-1]
-    length = len(movie.findAll("div", {"class": "oflow_a"}))
-    director = movie.findAll("div", {"class": "oflow_a"})[1].text.replace("\n", "") 
-    genre = movie.findAll("div", {"class": "oflow_a"})[length-1].text.replace("\n", "")
+    title = movie.find("h2").text.strip()
+    image = movie.find("img").get("data-src")
+    detail = movie.find("div", {"class": "synopsis"}).text.strip()
+    metaData = movie.find("div", {"class": "meta-body-item meta-body-info"})
+    metaDict = extract_meta(metaData) 
+    duration = metaDict["duration"]
+    date = metaDict["date"]
+    genre = metaDict["genre"]
+    director = movie.find("div", {"class": "meta-body-item meta-body-direction light"}).a.text 
     
     movie_dict = {"title": title, "image": image, "detail": detail, "date": date, "duration": duration, "director": director, "genre": genre}
     return movie_dict
+
+def extract_meta(metaData):
+    spanArray = metaData.findAll("span")
+    spacerCount = 0
+    spacer = "/"
+    defaultDuration = "Not Specified"
+    for span in spanArray:
+        if span.text == spacer:
+            spacerCount += 1
+    date = spanArray[0].text
+    duration = defaultDuration
+    genreArray = []
+    genre = ""
+    if spacerCount != 1:
+        duration = metaData.contents[4].strip()
+        for index in range(4, len(spanArray)):
+            genreArray.append(spanArray[index].text)
+        genre = ", ".join(genreArray)
+    else:
+        for index in range(2, len(spanArray)):
+            genreArray.append(spanArray[index].text)
+        genre = ", ".join(genreArray)
+    
+    return {"duration": duration, "date": date, "genre": genre}
+    
+        
 
 def main():
     url = "http://www.beyazperde.com/filmler/vizyondakiler/sinema-sayisi/"
     request = requests.get(url)
     soup = BeautifulSoup(request.text, "lxml")
-    movies = soup.findAll("div", {"class": "data_box"})
+    movies = soup.findAll("div", {"class": "card card-entity card-entity-list cf hred"})
     movies_list = [movie_details(movie) for movie in movies]
     print("[", end="")
     for index in range(len(movies_list)):
